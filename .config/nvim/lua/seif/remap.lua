5d3eb536eaf3b9c5a2dfa0971d2a78b0e3121346
Seifbarouni <seif.barouni11@gmail.com>
2022-08-23 18:01:16 +0100

    new remaps

--- .config/nvim/lua/seif/remap.lua
+++ .config/nvim/lua/seif/remap.lua
@@ -1,4 +1,5 @@
 local nnoremap = require("seif.keymap").nnoremap
+local inoremap = require("seif.keymap").inoremap
 
 nnoremap("<leader>pv","<cmd>Ex<CR>")
 nnoremap("<leader>wq","<cmd>wq<CR>")
@@ -7,3 +8,9 @@ nnoremap("<leader>fg","<cmd>Telescope live_grep<cr>")
 nnoremap("<leader>fb","<cmd>Telescope buffers<cr>")
 nnoremap("<leader>fh","<cmd>Telescope help_tags<cr>")
 nnoremap("<leader>fc","<cmd>Telescope git_commits<cr>")
+nnoremap("<leader>f","<cmd>lua vim.lsp.buf.formatting()<cr>")
+
+inoremap("(","()<left>")
+inoremap("{","{}<left>")
+inoremap("'","''<left>")
+inoremap("\"","\"\"<left>")
